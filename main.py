980de10665231b5ce6c360a364c48c64c5aed03c
Ruslan Galimullin <ruslan.gali.galimullin@yandex.ru>
2022-05-24 00:02:34 +0300

    1

--- main.py
+++ main.py
@@ -3,14 +3,17 @@
 def text_analysis(text):
     en_alphabet = 'abcdefghijklmnopqrstuvwxyz'
     dictionary = {}
+
     for letter in text:
         letter = letter.lower()
         if letter in en_alphabet:
             if letter not in dictionary:
                 dictionary[letter] = 0
             dictionary[letter] += 1
+
     result1 = sorted(dictionary.items(), key=lambda x: x[1], reverse=True)
     result1 = dict(result1)
+
     for k, v in result1.items():
         print(k.upper(), v)
 
@@ -18,6 +21,7 @@ def text_analysis(text):
 
     dictionary2 = {}
     words = text.lower().split()
+
     for word in words:
         if word[-1] not in en_alphabet:
             word = word[0:-1]
@@ -37,18 +41,20 @@ text_analysis('Hello world! hello')
 def cipher(text):
     en_alphabet = 'abcdefghijklmnopqrstuvwxyz'
     dictionary = {}
-    for i in range(26):
+
+    for _ in range(26):
         a = input().strip().lower().split()
         dictionary[a[0]] = a[1]
         dictionary[a[0].upper()] = a[1].upper()
-    print(dictionary)
 
     result = ''
+
     for symbols in text:
         if symbols.lower() not in en_alphabet:
             result += symbols
         else:
             result += dictionary[symbols]
+
     return result
 
 
